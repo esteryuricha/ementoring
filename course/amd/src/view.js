669fb8723294aa8564fc7367a50da73493f23937
irwanphan <irwanphan@gmail.com>
2021-12-09 11:51:42 +0700

    class management menu buttons

--- course/amd/src/view.js
+++ course/amd/src/view.js
@@ -46,4 +46,8 @@ export const init = () => {
         }
     });
     registered = true;
+
+    const main = document.getElementById('region-main')
+    const classMenu = document.getElementById('inst29')
+    main.prepend(classMenu)
 };
