bec254fc252224e0c0e160a023e854f9bc208076
irwanphan <irwanphan@gmail.com>
2021-12-11 19:52:02 +0700

    moved course info to main area

--- theme/moodio/javascript/custom_main.js
+++ theme/moodio/javascript/custom_main.js
@@ -1,5 +1,9 @@
 // change class management meniu position
 const regionMain = document.getElementById('region-main')
 const classMenu = document.getElementById('inst29')
+const courseInfo = document.getElementById('inst30')
+
+regionMain.prepend(courseInfo)
+courseInfo.classList.add('content-container')
 regionMain.prepend(classMenu)
 classMenu.classList.add('content-container')
