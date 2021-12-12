04ae832ce335bd9fc1949c90d12cca18869fff5b
irwanphan <irwanphan@gmail.com>
2021-12-12 18:07:55 +0700

    remove selected course form nav, move settings button to beside turn editing on, add validator for existence of selected element using js

--- theme/moodio/javascript/custom_main.js
+++ theme/moodio/javascript/custom_main.js
@@ -2,15 +2,37 @@
 const regionMain = document.getElementById('region-main')
 const classMenu = document.getElementById('inst29')
 const courseInfo = document.getElementById('inst30')
-
-regionMain.prepend(courseInfo)
-courseInfo.classList.add('content-container')
-regionMain.prepend(classMenu)
-classMenu.classList.add('content-container')
+if (regionMain) {
+    if (courseInfo) {
+        regionMain.prepend(courseInfo)
+        courseInfo.classList.add('content-container')
+    }
+    if (classMenu) {
+        regionMain.prepend(classMenu)
+        classMenu.classList.add('content-container')
+    }
+}
 
 const enrollUserButton = document.getElementById('enrolusersbutton-1')
 const participantForm = document.getElementById('participantsform')
-participantForm.prepend(enrollUserButton)
-enrollUserButton.classList.add('float-right')
+if (participantForm) {
+    if (enrollUserButton) {
+        participantForm.prepend(enrollUserButton)
+        if (enrollUserButton) enrollUserButton.classList.add('float-right')
+    }
+}
 
+// move settings button beside Turn editing on on administrator
+const courseHeader = document.getElementById('course-header')
+const actionMenuAdmin2 = document.getElementById('action-menu-2')
+if (courseHeader) {
+    if (actionMenuAdmin2) {
+        courseHeader.prepend(actionMenuAdmin2)
+    }
+}
 
+// remove selected course from nav drawer
+const courseHomeSelected = document.querySelector("[data-key='coursehome']")
+if (courseHomeSelected) {
+    courseHomeSelected.parentElement.remove()
+}
