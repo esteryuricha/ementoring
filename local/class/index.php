ee9deade25710fda61a99bd358086572c994d1f5
esteryuricha <esteryuricha@gmail.com>
2021-12-20 10:31:19 +0700

    view mentor at admin page

--- local/class/index.php
+++ local/class/index.php
@@ -21,10 +21,20 @@ echo $OUTPUT->header();
 $manager = new manager();
 $classes = $manager->get_classes();
 
+//get role
+$role_assignment = $DB->get_record_sql("select r.id from {role} r inner join {role_assignments} ra on r.id = ra.roleid where ra.userid=$USER->id");
+$view_mentor = true;
+//add menu class for mentor and participant
+if($role_assignment->id == 3 || $role_assignment->id == 5) {
+    $view_mentor = false;
+}
+
+
 $templatecontext = (object)[
     'classes' => array_values($classes),
     'addUrl' => new moodle_url($CFG->wwwroot.'/local/class/editclass.php'),
-    'viewUrl' => new moodle_url($CFG->wwwroot.'/course/view.php')
+    'viewUrl' => new moodle_url($CFG->wwwroot.'/course/view.php'),
+    'viewMentor' => $view_mentor
 ];
 
 echo $OUTPUT->render_from_template('local_class/table', $templatecontext);
